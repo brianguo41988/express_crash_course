8b374d50d5bdb6b0c7c6aafc1862f65946870c58
brianguo41988 <brianguo@live.unc.edu>
2021-06-28 13:10:11 -0400

    added button click js

--- buttonclick.js
+++ buttonclick.js
@@ -1,9 +1,13 @@
-async function signUpFn(){
-    const result = await axios({	
-        method: 'post',	
-        url: 'https://express41988.herokuapp.com/button',	
-        withCredentials: true,	
-        data: {	
-          "message": 'button clicked'
-        },	
-      })};
+console.log("entered button click");
+
+async function button1(){
+    console.log("HELLO");
+    // const result = await axios({	
+    //     method: 'post',	
+    //     url: 'https://express41988.herokuapp.com/button',	
+    //     withCredentials: true,	
+    //     data: {	
+    //       "message": 'button clicked'
+    //     },	
+    //   })
+    };
