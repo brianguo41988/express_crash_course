8d5236df1f1e5b31f2fdddbd917db6eb72fadbd1
brianguo41988 <brianguo@live.unc.edu>
2021-06-28 12:41:09 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("hi");
+const express = require('express');
+const path = require('path');
+const app = express();
+console.log("require is working");
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/1', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index1.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => console.log(`Sever started on port ${PORT}`));
